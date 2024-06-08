419841fcc5095964c45db842d349d876a78117a6
Luiz Fernando <kakasten.cipher@gmail.com>
2024-06-08 20:11:33 -0300

    Including main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+extern crate panic_halt;
+extern crate embedded_hal;
+extern crate rp2040_hal;
+
+use panic_halt as _;
+use rp2040_hal as hal;
+use hal::pac;
+use embedded_hal::digital::v2::{OutputPin, InputPin};
+use rp2040_hal::clocks::Clock;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[rp2040_hal::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = pac::Peripherals::take().unwrap();
+    let _core = pac::CorePeripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let _clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut closing_signal_pin = pins.gpio21.into_push_pull_output();
+
+    let button_pin = pins.gpio16.into_pull_up_input();
+    let locked_door_pin = pins.gpio17.into_pull_up_input();
+
+    loop {
+        closing_signal_pin.set_low().unwrap();
+        while button_pin.is_low().unwrap(){
+            closing_signal_pin.set_high().unwrap();
+        }
+        if locked_door_pin.is_low().unwrap(){
+            while button_pin.is_low().unwrap() {
+                closing_signal_pin.set_high().unwrap();
+            }
+        }
+    }
 }
